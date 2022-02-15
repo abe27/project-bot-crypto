18cd79deeebab8a672e4ea2ce7838c011af1242c
Taweechai Yuenyang <krumii.it@gmail.com>
2022-02-15 15:07:44 +0700

    เป็ยนชื่อ

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Crypto Bot Application'),
 
     /*
     |--------------------------------------------------------------------------
