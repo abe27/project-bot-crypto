cced4b90182c16d95e29ca8d4be7233cc769dff4
Taweechai Yuenyang <krumii.it@gmail.com>
2022-02-15 16:55:00 +0700

    เพิ่ม model user และ activities

---
+++ app/Http/Controllers/ActivitiesLogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ActivitiesLog;
+use Illuminate\Http\Request;
+
+class ActivitiesLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ActivitiesLog  $activitiesLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ActivitiesLog $activitiesLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ActivitiesLog  $activitiesLog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ActivitiesLog $activitiesLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ActivitiesLog  $activitiesLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ActivitiesLog $activitiesLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ActivitiesLog  $activitiesLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ActivitiesLog $activitiesLog)
+    {
+        //
+    }
+}
